543b8610303e1fca04e180e6fbd24a6439865ecb
Lorez <juanda2915@gmail.com>
2025-10-09 00:37:51 -0500

    Cambio

--- CodigoBaseTuring.py
+++ CodigoBaseTuring.py
@@ -4,73 +4,55 @@ def TuringMachine(states,input,start,alph):
     actval=input[i]
     insidealph(actval,alph)
     lop=True
+    count=len(states[start][actval])
     change=True
     while lop:
         if states[start]=="E":
                 print(input)
                 sys.exit(0)
+        print("iteracion")
+        input,start,actval,i=TuringLoop(states,input,start,alph,actval,i)
 
-        for x in range(0,len(states[start][actval])):#start es el estado ejecutando, actval el valor activo
-            print(input,actval,i)
-            print(x, "indice")
 
-            if states[start][actval][x]=="w":
-                print("w","entro",states[start][actval][x+1])
-                input=replacement(input,i,states[start][actval][x+1])
-                if len(states[start][actval])>x+2:
-                    x=x+2
-                    change=False
+
+            
+def TuringLoop(states,input,start,alph,actval,i):
+    actval1=""
+    for x in range(0,len(states[start][actval])):#start es el estado ejecutando, actval el valor activo
+        print(input,start,actval,x,i)
+
+        if states[start][actval][x] in states:
+            start=states[start][actval][x]
+            actval=actval1
+
+        if states[start][actval][x]=="w":
+            #print("w",states[start][actval][x+1])
+            input=replacement(input,i,states[start][actval][x+1])
+            #print(input)
+            insidealph(actval,alph)
+            actval=blankswap(input,i)
+            
+        elif states[start][actval][x]=="R":
+            print("r")
+            if states[start][actval][x]==states[start][actval][-1]:
+                i=i+1
+                insidealph(actval,alph)
+                actval=blankswap(input,i)
+
+        elif states[start][actval][x]=="L":
+            print("l")
+
+            if states[start][actval][x]!=states[start][actval][-1]:# Si L Q1
+                i=i-1
+                insidealph(actval,alph)
+                actval1=blankswap(input,i)
+            else:#Si L
+                i=i-1
                 insidealph(actval,alph)
                 actval=blankswap(input,i)
             
-            if states[start][actval][x]=="R":
-                print("r")
-                try:
-                    print(states[start][actval][x+1])
-                except IndexError:
-                        i=i+1
-                        insidealph(actval,alph)
-                        actval=blankswap(input,i)
 
-                else:
-                    if ((states[start][actval][x+1]) in states) and change:
-                        i=i+1
-                        start=states[start][actval][x+1]                        
-                        insidealph(actval,alph)
-                        actval=blankswap(input,i)
-                    else:
-                        i=i+1
-                        insidealph(actval,alph)
-                        actval=blankswap(input,i)
-                        if not change:
-                            x=x-2
-                            change=True
-
-            elif states[start][actval][x]=="L":
-                print("l")
-                try:
-                    vol=states[start][actval][x+1]
-                except IndexError:
-                        i=i-1
-                        print(i,"error")
-                        insidealph(actval,alph)
-                        actval=blankswap(input,i)
-                else:
-                    if (states[start][actval][x+1]) in states and change:
-                        i=i-1
-                        start=states[start][actval][x+1]
-                        insidealph(actval,alph)
-                        actval=blankswap(input,i)
-                        print(start,"estado cam")
-                    else:
-                        i=i-1
-                        print(i,"noer")
-                        insidealph(actval,alph)
-                        actval=blankswap(input,i)
-                        if not change:
-                            x=x-2
-                            change=True
-
+    return input,start,actval,i
 
 def blankswap(input,dat1):#input es el dato inicial, dat1 es el indice actual
     if dat1<0:
@@ -104,7 +86,7 @@ states={
     "q0":{"0":["R"],"1":["R"],"_":["L","q1"]},
     "q1":{"1":["w","0","L"],"0":["w","1","L","qf"],"_":["w","1","L","qf"]},
     "qf":"E"}
-input="0001"
+input="011"
 start="q0" 
 alph=["0","1","_"]
 print("inicio")
