c6cefc8d98dafa33d2a6b685528217cae18440b6
Lorez <juanda2915@gmail.com>
2025-10-09 12:41:14 -0500

    funcional

--- CodigoBaseTuring.py
+++ CodigoBaseTuring.py
@@ -4,34 +4,40 @@ def TuringMachine(states,input,start,alph):
     actval=input[i]
     insidealph(actval,alph)
     lop=True
-    count=len(states[start][actval])
-    change=True
+    actval1=""
+    change=False
     while lop:
         if states[start]=="E":
                 print(input)
                 sys.exit(0)
         print("iteracion")
-        input,start,actval,i=TuringLoop(states,input,start,alph,actval,i)
-
-
-
+        if change:
+            actval=actval1
+            change=False
+        
+        input,start,actval,i,change,actval1=TuringLoop(states,input,start,alph,actval,i,change)
             
-def TuringLoop(states,input,start,alph,actval,i):
+def TuringLoop(states,input,start,alph,actval,i,change):
     actval1=""
     for x in range(0,len(states[start][actval])):#start es el estado ejecutando, actval el valor activo
-        print(input,start,actval,x,i)
+        print("\n",input,start,"valor",actval,"iteracion",x,"indice",i)
+        print("actval",states[start][actval][x])
 
         if states[start][actval][x] in states:
+            print("s")
             start=states[start][actval][x]
-            actval=actval1
+            if actval1!="":
+                actval=actval1
+            return input,start,actval,i,change,actval1
 
         if states[start][actval][x]=="w":
             #print("w",states[start][actval][x+1])
+            print("w")
             input=replacement(input,i,states[start][actval][x+1])
             #print(input)
             insidealph(actval,alph)
             actval=blankswap(input,i)
-            
+        
         elif states[start][actval][x]=="R":
             print("r")
             if states[start][actval][x]==states[start][actval][-1]:
@@ -46,24 +52,24 @@ def TuringLoop(states,input,start,alph,actval,i):
                 i=i-1
                 insidealph(actval,alph)
                 actval1=blankswap(input,i)
+                print("l especial")
+                change=True
             else:#Si L
                 i=i-1
                 insidealph(actval,alph)
                 actval=blankswap(input,i)
-            
 
-    return input,start,actval,i
+    return input,start,actval,i,change,actval1
 
-def blankswap(input,dat1):#input es el dato inicial, dat1 es el indice actual
-    if dat1<0:
+def blankswap(input,index):#input es el dato inicial, dat1 es el indice actual
+    datoactual=""
+    try:
+        input[index]
+    except IndexError:
         datoactual="_"
     else:
-        try:
-            input[dat1]
-        except IndexError:
-            datoactual="_"
-        else:
-            datoactual=input[dat1]
+        datoactual=input[index]
+
     return datoactual #retorna el dato actualmente usado
 
 def insidealph(value,alph):
@@ -75,19 +81,52 @@ def insidealph(value,alph):
 
 def replacement(input,index,val):
     ninp=""
-    for x in range(0,len(input)):
-        if x==index:
-            ninp=ninp+val
+    added=""
+    if index<0:
+        for y in range(0,abs(index)):
+            print(y)
+            if y==0:
+                ninp=val
+            else:
+                added=added+"_"
+        ninp=val+added
+        index=index-len(input)
+            
+    elif index>(len(input)-1):
+        for y in range(0,(index-(len(input)-1))):
+            if y==(index-(len(input))):
+                ninp=val
+            else:
+                added=added+"_"
+        ninp=added+ninp
+    for x in range(0,len(input)):#input 1001, etc
+        try:
+            vo=input[index]
+        except IndexError:
+            if index<0:
+                ninp=ninp+input
+                return ninp
+            elif index>(len(input)-1):
+                ninp=input+ninp
+                return ninp
         else:
-            ninp=ninp+input[x]
+            if x==index:
+                ninp=ninp+val
+            else:
+                ninp=ninp+input[x]
     return ninp
 
+#states={
+#    "q0":{"0":["R"],"1":["R"],"_":["L","q1"]},
+#    "q1":{"0":["w","1","L"],"1":["w","0","L","qf"],"_":["w","0","L","qf"]},
+#    "qf":"E"}
 states={
-    "q0":{"0":["R"],"1":["R"],"_":["L","q1"]},
-    "q1":{"1":["w","0","L"],"0":["w","1","L","qf"],"_":["w","1","L","qf"]},
+    "q0":{"0":["R"],"1":["R","q1"],"_":["R","qf"]},
+    "q1":{"0":["R","q2"],"1":["R","q0"],"#":["R"]},
+    "q2":{"0":["R","q1"],"1":["R"],"#":["R"]},
     "qf":"E"}
-input="011"
+input="10#01"
 start="q0" 
-alph=["0","1","_"]
+alph=["0","1","_","#"]
 print("inicio")
 TuringMachine(states,input,start,alph)
